f3a89b253d75c0db1d8852103e421e60d79285b5
chaeryeongsdog <xian920626@gmail.com>
2025-01-19 15:31:51 +0800

    net7.0

--- obj/Debug/net7.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net7.0/MyApi.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d99949034ca334e69fee2aec5bbd428ecea3709")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
